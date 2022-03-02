00f790d64b6f534701065e164a9ce607c2eee913
Leidson <o.q.leidson@gmail.com>
2022-03-01 21:35:00 -0300

    Commit inicial

---
+++ src/main/java/br/com/leidson/projetofinalmudulo08letscode/ProjetoFinalMudulo08LetscodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.leidson.projetofinalmudulo08letscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalMudulo08LetscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalMudulo08LetscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leidson/projetofinalmudulo08letscode/ProjetoFinalMudulo08LetscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leidson.projetofinalmudulo08letscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalMudulo08LetscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
